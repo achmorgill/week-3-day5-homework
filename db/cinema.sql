4edf551b024a71c506bd0419777992f46090dbd4
user <user@CODECLAN013.local>
2017-04-28 15:12:03 +0100

    week 3 day 5 homework

---
+++ db/cinema.sql
@@ -0,0 +1,21 @@
+DROP TABLE tickets;
+DROP TABLE customer;
+DROP TABLE films;
+
+CREATE TABLE customers (
+name VARCHAR(255),
+funds INT2,
+id SERIAL4 PRIMARY KEY
+);
+
+CREATE TABLE films (
+title VARCHAR(255),
+price INT2,
+id SERIAL4 PRIMARY KEY
+);
+
+CREATE TABLE tickets (
+customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
+film_id INT4 REFERENCES films(id) ON DELETE CASCADE,
+id SERIAL4 PRIMARY KEY
+);
---
+++ models/customer.rb
@@ -0,0 +1,34 @@
+require_relative('../db/sqlrunner.rb')
+
+class Customer
+
+  attr_reader :id
+  attr_accessor :name, :funds
+
+  def initialize(options)
+    @name = options['name']
+    @funds = options['funds']
+    @id = options['id'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO customers (name, funds) VALUES ('#{@name}', '#{@funds}') RETURNING *"
+    result = SqlRunner.run(sql).first
+    @id = result['id'].to_i
+    return @id
+  end
+
+  def Customer.all()
+    sql = "SELECT * FROM customers"
+    return get_many_rows(sql)
+  end
+
+  def Customer.get_many_rows(sql)
+    customers = SqlRunner.run(sql)
+    result = customers.map { |customer| Customer.new(customer)}
+    return result
+  end
+
+end
+
+
